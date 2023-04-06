19f0c77139d01f64a4582fed73091e45f7c4de41
Crabbex <80641154+Crabbex@users.noreply.github.com>
2023-04-07 01:39:58 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+path = r"Path to my picture"
+pathOut = r"Path to where I want the edited photo to go"
+
+Tk().withdraw()
+filename = askopenfilename()
+
+for filename in os.listdir(path):
+    img = Image.open(fr"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN).convert("L")
+
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f"{pathOut}/{clean_name}_edited.jpg")
